278005cb485fc0c3b475c7cd25de780de05b17b5
CLS-project <weiguang.cui@uam.es>
2025-04-11 14:04:24 +0200

    specify the output dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import svelte from '@astrojs/svelte'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.CLS-project.github.io',
+  outDir: './dist',
   integrations: [
     tailwind(),
     icon({
